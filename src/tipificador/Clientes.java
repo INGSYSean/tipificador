1b51c23756c83befb981f63940a9c7dc03fc43e2
yoe81 <Yoe@DESKTOP-S83AM13>
2016-02-24 20:38:14 -0500

    vers2

---
+++ src/tipificador/Clientes.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipificador;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author Yoe
+ */
+@Entity
+@Table(name = "clientes", catalog = "tipificador", schema = "")
+@NamedQueries({
+    @NamedQuery(name = "Clientes.findAll", query = "SELECT c FROM Clientes c"),
+    @NamedQuery(name = "Clientes.findByIdcliente", query = "SELECT c FROM Clientes c WHERE c.idcliente = :idcliente"),
+    @NamedQuery(name = "Clientes.findByNombre", query = "SELECT c FROM Clientes c WHERE c.nombre = :nombre"),
+    @NamedQuery(name = "Clientes.findByApellido", query = "SELECT c FROM Clientes c WHERE c.apellido = :apellido"),
+    @NamedQuery(name = "Clientes.findByTelefono", query = "SELECT c FROM Clientes c WHERE c.telefono = :telefono"),
+    @NamedQuery(name = "Clientes.findByGenero", query = "SELECT c FROM Clientes c WHERE c.genero = :genero")})
+public class Clientes implements Serializable {
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "idcliente")
+    private String idcliente;
+    @Basic(optional = false)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @Column(name = "apellido")
+    private String apellido;
+    @Basic(optional = false)
+    @Column(name = "telefono")
+    private String telefono;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "direccion")
+    private String direccion;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "email")
+    private String email;
+    @Basic(optional = false)
+    @Lob
+    @Column(name = "ciudad")
+    private String ciudad;
+    @Basic(optional = false)
+    @Column(name = "genero")
+    private String genero;
+
+    public Clientes() {
+    }
+
+    public Clientes(String idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public Clientes(String idcliente, String nombre, String apellido, String telefono, String direccion, String email, String ciudad, String genero) {
+        this.idcliente = idcliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.email = email;
+        this.ciudad = ciudad;
+        this.genero = genero;
+    }
+
+    public String getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(String idcliente) {
+        String oldIdcliente = this.idcliente;
+        this.idcliente = idcliente;
+        changeSupport.firePropertyChange("idcliente", oldIdcliente, idcliente);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        String oldNombre = this.nombre;
+        this.nombre = nombre;
+        changeSupport.firePropertyChange("nombre", oldNombre, nombre);
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        String oldApellido = this.apellido;
+        this.apellido = apellido;
+        changeSupport.firePropertyChange("apellido", oldApellido, apellido);
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        String oldTelefono = this.telefono;
+        this.telefono = telefono;
+        changeSupport.firePropertyChange("telefono", oldTelefono, telefono);
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        String oldDireccion = this.direccion;
+        this.direccion = direccion;
+        changeSupport.firePropertyChange("direccion", oldDireccion, direccion);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        String oldEmail = this.email;
+        this.email = email;
+        changeSupport.firePropertyChange("email", oldEmail, email);
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        String oldCiudad = this.ciudad;
+        this.ciudad = ciudad;
+        changeSupport.firePropertyChange("ciudad", oldCiudad, ciudad);
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        String oldGenero = this.genero;
+        this.genero = genero;
+        changeSupport.firePropertyChange("genero", oldGenero, genero);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idcliente != null ? idcliente.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clientes)) {
+            return false;
+        }
+        Clientes other = (Clientes) object;
+        if ((this.idcliente == null && other.idcliente != null) || (this.idcliente != null && !this.idcliente.equals(other.idcliente))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "tipificador.Clientes[ idcliente=" + idcliente + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
